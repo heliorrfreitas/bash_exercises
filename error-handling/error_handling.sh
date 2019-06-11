66159fd658d7aea9d45538ebe6c968e38e55a090
heliorrfreitas <heliorui.ramosfreitas@gus.pittstate.edu>
2019-06-11 16:52:35 -0500

    Re-factor error-handling/error_handling_sh

--- error-handling/error_handling.sh
+++ error-handling/error_handling.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
 function main {
-    if [ $# -eq 0 ] || [ $# -ge 2 ];
+    if [[ $# -ne 1 ]];
     then
         echo "Usage: ./error_handling <greetee>"
         exit 1
-    else
-        name="$1"
-        echo "Hello, $name"
     fi
+    name="$1"
+    echo "Hello, $name"
 }
 
 main "$@"
